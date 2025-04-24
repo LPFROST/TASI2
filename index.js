dd6638c294b7fbbc233613acd9753d3f4065a104
LEANDRO SOBRAL SILVEIRA <leandro.sobral@cs.cruzeirodosul.edu.br>
2025-04-24 09:55:01 -0300

    MELHOR AGR

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 let express = require('express');
 let app = express();
-let db = require('./db');
-app.use(express.json())
-const porta = 3333;
+const db = require('./db');
+app.use(express.json());
+const PORT = 3333;
 
-app.listen(porta, ()=>  {
-    console.log("Servidor rodando na porta " + porta)
+app.get('/livros/:id', async function(req, res){
+    const { id } = req.params;
+    console.log(id)
+    try{
+        const [rows] = await db.execute('SELECT * FROM livros WHERE ID_livro = ?', [id]);
+        if (rows.length > 0) {
+            res.status(202).json(rows[0])
+        } else {
+            res.status(404).json({message: 'Livro não encontrado'})
+        }
+    } catch (error){
+        res.status(500).json({error: 'Erro interno do servidor'})
+    }
 })
 
-app.get('/livros/:id', async function (req, res) {
-    res.send('rota livros ID');
-});
+app.get('/livros', async function(req, res){
+    try{
+        const [rows] = await db.execute('SELECT ID_livro, titulo, preco FROM livros');
+        res.status(202).json(rows);
+    } catch (error){
+        console.log('Erro ao listar livros: ' , error)
+        res.status(500).json({error: 'Erro interno do servidor'})
+    }
+})
 
-app.get('/livros', async function (req, res) {
-    res.send('rota livros');
-});
-
-app.post('/livros', async function (req, res) {
-    const { titulo, preco} = req.body;
-
-    try {
-        const[result] = await db.execute('INSERT INTO livros (titulo, preco) VALUES (?,?)', [titulo, preco])
-        res.json({
-            "sucesso": true,
-            "mensagem":"Livro Inserido"})
-    } catch (error) {
-        res.json({error:"Erro interno do servidor"})
+app.post('/livros',async function(req,res){
+    const { titulo, preco } = req.body
+    console.log(titulo, preco);
+    try{
+        const [result] = await db.execute('INSERT INTO livros (titulo, preco) VALUES (?, ?)',[titulo, preco]);
+        console.log(result.insertId)
+        res.status(202).json({
+            "sucesso" : true,
+            "mensagem" : "livro inserido com sucesso",
+            "id_inserido" : result.insertId
+        })
+    }catch(error){
+        console.log("erro ao criar livro", error);
+        res.status(345).json({error: 'Error internor dos servirdor'})
     }
 });
 
-app.put('/livros/:id', async function (req, res) {
-    res.send('rota put ID');
+app.put('/livros',function(req, res){
+    res.send('ROTA GET LIVROS PUT')
 });
 
-app.delete('/livros/:id', async function (req, res) {
-    res.send('rota delete ID');
+app.delete('/livros',function(req, res){
+    res.send('ROTA GET LIVROS DELETE')
+});
+
+app.listen(PORT, ()=> {
+    console.log('Projeto iniciado na porta 3000')
 });
